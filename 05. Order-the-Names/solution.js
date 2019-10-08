d8c2c50abb6227e9374dc2d4242e6242f33f43d8
georgi-nikoloff <georgiinikoloff@gmail.com>
2019-10-08 22:39:43 +0300

    Updated

--- 05. Order-the-Names/solution.js
+++ 05. Order-the-Names/solution.js
@@ -8,11 +8,11 @@ function solve() {
         const firstLetter = name.split('')[0]
         const lowerCaseName = name.slice(1, name.length).toLowerCase()
         const result = firstLetter + lowerCaseName
-        let i = name.charCodeAt() - 65;
-        if (listElements[i].textContent.length > 0) {
-            listElements[i].textContent += ', ' + result
-        } else {
+        const i = name.charCodeAt() - 65;
+        if (listElements[i].textContent.length === 0) {
             listElements[i].textContent = result
+        } else {
+            listElements[i].textContent += ', ' + result
         }
     })
 }
